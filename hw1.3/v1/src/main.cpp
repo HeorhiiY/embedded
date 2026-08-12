9dd0fe26cd2aa606602cc5d9fce82644bebf2bf9
Heorhii <heorhii.yehiazarian@nextnano.com>
2026-08-12 23:21:00 +0200

    fix version naming

--- hw1.3/v1/src/main.cpp
+++ hw1.3/v1/src/main.cpp
@@ -1,22 +1,35 @@
 #include <Arduino.h>
 
-#define LED_PIN 5
-#define delayMs 1000
+#define LED_RED_PIN 5
+#define LED_BLUE_PIN 18
+
 
 void setup() {
-  pinMode(LED_PIN, OUTPUT);
-  // set the pin to low
-  digitalWrite(LED_PIN, LOW);
+  pinMode(LED_RED_PIN, OUTPUT);
+  pinMode(LED_BLUE_PIN, OUTPUT);
+  // set both pins to low
+  digitalWrite(LED_RED_PIN, LOW);
+  digitalWrite(LED_BLUE_PIN, LOW);
 
 }
 
 void loop() {
-
-  digitalWrite(LED_PIN, HIGH);
-  delay(delayMs);
-  digitalWrite(LED_PIN, LOW);
-  delay(delayMs);
-
-
+  // blink with varying frequency, gradually from 1 second to 50 milliseconds, then from 50 to 10 ms
+  for (int delayMs = 1000; delayMs >= 50; delayMs -= 50) {
+    digitalWrite(LED_RED_PIN, HIGH);
+    delay(delayMs);
+    digitalWrite(LED_RED_PIN, LOW);
+    digitalWrite(LED_BLUE_PIN, HIGH);
+    delay(delayMs);
+    digitalWrite(LED_BLUE_PIN, LOW);
+  }
+  for (int delayMs = 50; delayMs >= 10; delayMs -= 5) {
+    digitalWrite(LED_RED_PIN, HIGH);
+    delay(delayMs);
+    digitalWrite(LED_RED_PIN, LOW);
+    digitalWrite(LED_BLUE_PIN, HIGH);
+    delay(delayMs);
+    digitalWrite(LED_BLUE_PIN, LOW);
+  }
 }
 
--- hw1.3/v2/src/main.cpp
+++ hw1.3/v2/src/main.cpp
@@ -1,35 +1,22 @@
 #include <Arduino.h>
 
-#define LED_RED_PIN 5
-#define LED_BLUE_PIN 18
-
+#define LED_PIN 5
+#define delayMs 1000
 
 void setup() {
-  pinMode(LED_RED_PIN, OUTPUT);
-  pinMode(LED_BLUE_PIN, OUTPUT);
-  // set both pins to low
-  digitalWrite(LED_RED_PIN, LOW);
-  digitalWrite(LED_BLUE_PIN, LOW);
+  pinMode(LED_PIN, OUTPUT);
+  // set the pin to low
+  digitalWrite(LED_PIN, LOW);
 
 }
 
 void loop() {
-  // blink with varying frequency, gradually from 1 second to 50 milliseconds, then from 50 to 10 ms
-  for (int delayMs = 1000; delayMs >= 50; delayMs -= 50) {
-    digitalWrite(LED_RED_PIN, HIGH);
-    delay(delayMs);
-    digitalWrite(LED_RED_PIN, LOW);
-    digitalWrite(LED_BLUE_PIN, HIGH);
-    delay(delayMs);
-    digitalWrite(LED_BLUE_PIN, LOW);
-  }
-  for (int delayMs = 50; delayMs >= 10; delayMs -= 5) {
-    digitalWrite(LED_RED_PIN, HIGH);
-    delay(delayMs);
-    digitalWrite(LED_RED_PIN, LOW);
-    digitalWrite(LED_BLUE_PIN, HIGH);
-    delay(delayMs);
-    digitalWrite(LED_BLUE_PIN, LOW);
-  }
+
+  digitalWrite(LED_PIN, HIGH);
+  delay(delayMs);
+  digitalWrite(LED_PIN, LOW);
+  delay(delayMs);
+
+
 }
 
